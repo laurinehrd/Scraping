13c716bfea46d0afd740179cced60d36b03e74f1
Laurinehrd <laurine.hrd@hotmail.fr>
2021-02-01 09:20:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Scrap</title>
+
+  <link href="public/css/style.css" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+
+
+
+
+
+
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+    <!-- <script src="main.js"></script> -->
+
+</body>
+</html>
