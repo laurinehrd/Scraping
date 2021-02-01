da1bd399de2391bf21e1cd8506f545d7528fbcf3
Laurinehrd <laurine.hrd@hotmail.fr>
2021-02-01 14:47:42 +0100

    create front for form of homepage

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
 
     ?>
 
-    <div class="section-home">
+    <!-- page home -->
+    <!-- <div class="section-home">
 
         <div class="content col-md-6">
 
@@ -35,18 +36,103 @@
                 <button type="button" class="btn btn-outline-primary">S'inscrire</button>
             </div>
             
-
         </div>
 
-        <div class="image col-md-6">
-            <!-- <img src="assets/images/scrap.png" alt=""> -->
+        <div class="image col-md-6"></div>
 
+    </div> -->
+    <!-- fin page home -->
+
+
+    <!-- page se connecter -->
+    <!-- <div class="section-home">
+
+        <div class="content col-md-6">
+
+            <div class="logo mx-auto">
+                <img src="assets/images/logoScrap.png" alt="logo Scrap">
+                <h1>Scrap</h1>
+            </div>
+            <div class="accessAccount">
+                <h2>Se connecter</h2>
+
+                <form action="" method="post">
+                    <div class="form-floating mb-4">
+                        <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com">
+                        <label for="floatingEmail">Adresse mail</label>
+                    </div>
+                    <div class="form-floating">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Mot de passe</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-outline-primary">Se connecter</button>
+                </form>
+
+            </div>
+            
         </div>
+
+        <div class="image col-md-6"></div>
+
+    </div> -->
+    <!-- fin page se connecter -->
+
+
+
+    <!-- page créer un compte -->
+    <div class="section-home">
+
+        <div class="content col-md-6">
+
+            <div class="logo mx-auto">
+                <img src="assets/images/logoScrap.png" alt="logo Scrap">
+                <h1>Scrap</h1>
+            </div>
+            <div class="accessAccount">
+                <h2>Créer un compte</h2>
+
+                <form action="" method="post">
+
+                    <div class="row g-2">
+                        <div class="col-md">
+                            <div class="form-floating mb-4">
+                                <input type="name" class="form-control" id="floatingFirstname" placeholder="Firstname">
+                                <label for="floatingFirstname">Prénom</label>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="form-floating mb-4">
+                                <input type="name" class="form-control" id="floatingName" placeholder="Name">
+                                <label for="floatingName">Nom</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-floating mb-4">
+                        <input type="email" class="form-control" id="floatingEmail" placeholder="name@example.com">
+                        <label for="floatingEmail">Adresse mail</label>
+                    </div>
+                    <div class="form-floating mb-4">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                        <label for="floatingPassword">Mot de passe</label>
+                    </div>
+                    <div class="form-floating mb-4">
+                        <input type="password" class="form-control" id="floatingPasswordConfirm" placeholder="Password">
+                        <label for="floatingPasswordConfim">Confirmation du mot de passe</label>
+                    </div>
+
+                    <button type="submit" class="btn btn-outline-primary">S'inscrire</button>
+                </form>
+
+            </div>
+            
+        </div>
+
+        <div class="image col-md-6"></div>
 
     </div>
-
-
-
+    <!-- fin page créer un compte -->
 
 
 
