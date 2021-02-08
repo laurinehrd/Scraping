8c47d85c5c080e2a2d87a5b0c37bcdac890709da
Laurinehrd <laurine.hrd@hotmail.fr>
2021-02-08 21:25:55 +0100

    fix emailexist and passwordconfirm verifications

--- controller/signOnController.php
+++ controller/signOnController.php
@@ -9,23 +9,38 @@ class SignOnController
 {
     public function signOn()
     {
-        if(isset($_POST['createname']) && isset($_POST['createfirstname']) && isset($_POST['createemail']) && isset($_POST['createpassword']) && $_POST['passwordConfirm'])
-        {
-            
-                $user = new UserModel();
-                $result = $user->signOn($_POST['createname'], $_POST['createfirstname'], $_POST['createemail'], $_POST['createpassword']);
+        if(isset($_POST['createname']) && !empty($_POST['createname'])
+        && isset($_POST['createfirstname']) && !empty($_POST['createfirstname'])
+        && isset($_POST['createemail']) && !empty($_POST['createemail'])
+        && isset($_POST['createpassword']) && !empty($_POST['createpassword'])
+        && isset($_POST['passwordConfirm']) && !empty($_POST['passwordConfirm'])
+        ){
     
-                $errorMdp = false;
+            $errorMdp = false;
 
-                if(count($result) === 0)
+            $user = new UserModel();
+
+            $check = $user->emailExist($_POST['createemail']); // récupérer tous les emails pour voir si l'email existe
+
+            if(count($check) === 0) // compte le nb de fois ou l'email apparait
+            {
+
+                if($_POST['createpassword'] == $_POST['passwordConfirm']) // si le password confirm est bien écrit
                 {
+                    $result = $user->signOn($_POST['createname'], $_POST['createfirstname'], $_POST['createemail'], $_POST['createpassword']);
                     header('Location: ?action=dashboard');
-                    
                 }
                 else {
-                    $error = true;
-                    exit();
+                    echo 'veuillez confirmer le bon mot de passe';
                 }
+ 
+            }
+            else {
+                $error = true;
+                // header('Location: ?action=home');
+                echo 'Votre email est déjà présent dans la base de données, veuillez vous connecter.';
+                exit();
+            }
 
                 // if($row == 0)
                 // {
