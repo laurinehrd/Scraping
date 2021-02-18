01c95787762b997974cfb882ea56a7078d74454c
laurine herard <laurine.hrd@hotmail.fr>
2021-02-18 19:18:03 +0100

    doing update scrap

--- controller/listScrapController.php
+++ controller/listScrapController.php
@@ -1,14 +1,59 @@
 <?php
 
-require_once 'model/model.php';
 require_once 'view/view.php';
+
 
 
 class ListScrapController 
 {
     public function listScrap()
     {
+
+        $scrap = new ScrapModel();
+        $liste = $scrap->listScrap(); 
+
+        // suppression de l'extraction
+        if(isset($_POST['deleteScrap']) && !empty($_POST['deleteScrap']))
+        {
+            $check = new CheckController();
+            $idScrap = $check->check($_POST['idScrap']);
+
+            $delete = new ScrapModel();
+            $delete->deleteScrap($idScrap);
+            $delete->deleteDetailScrap($idScrap);
+
+            header('Location: ?action=listScrap');
+        }
+        // modification des informations
+        // if(isset($_POST['']) && !empty($_POST['']))
+        // {
+        //     $check = new CheckController();
+        //     $idScrap = $check->check($_POST['idScrap']);
+
+        //     $delete = new ScrapModel();
+        //     $delete->updateScrap();
+
+        //     $_SESSION[""] = [
+        //         "id"=> $id,
+        //         "titre"=> $titre,
+        //         "descrea"=> $descrea,
+        //         "img"=> $img,
+        //         "contexte"=> $contexte,
+        //         "choix"=> $choix
+        //     ];
+
+            // $view = new View('updateScrap');
+            // $view->generate(array('listes'=>$liste));
+
+            // dans la nouvelle vue avec le form pour modifier le scrap, ne pas oublier de mettre un input hidden pour l'id
+        }
+
+
+
+
+
+
         $view = new View('listScrap');
-        $view->generate(array());
+        $view->generate(array('listes'=>$liste));
     }
 }
